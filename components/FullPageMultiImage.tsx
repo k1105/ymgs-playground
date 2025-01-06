9511993f274668e78963cc4fa124cb3b035e5a96
k1105 <kanapon1105@yahoo.co.jp>
2025-01-06 23:23:03 +0900

    画像がロードされてから表示するように修正

--- components/FullPageMultiImage.tsx
+++ components/FullPageMultiImage.tsx
@@ -1,13 +1,26 @@
+import { useState } from "react";
+
 export const FullPageMultiImage = ({
   transitionProgress = 0,
 }: {
   transitionProgress?: number;
 }) => {
+  const [loaded, setLoaded] = useState<boolean>(false);
   return (
     <>
-      <div className="image-container">
-        <img src="/img/lag/lag1.jpg" />
-        <img src="/img/lag/lag2.jpg" />
+      <div className={`image-container ${loaded && "active"}`}>
+        <img
+          src="/img/lag/lag1.jpg"
+          onLoad={() => {
+            setLoaded(true);
+          }}
+        />
+        <img
+          src="/img/lag/lag2.jpg"
+          onLoad={() => {
+            setLoaded(true);
+          }}
+        />
       </div>
       <style jsx>{`
         .image-container {
@@ -17,11 +30,16 @@ export const FullPageMultiImage = ({
           display: flex;
           gap: 2vw;
           margin: 0 auto;
+          opacity: 0;
+          transition: all 0.5s;
           img {
             width: 45vw;
             height: 100%;
             object-fit: contain;
           }
+        }
+
+        .image-container.active {
           opacity: ${1 - Math.abs(transitionProgress) / 100};
         }
 
