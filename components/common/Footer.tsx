ad70153aac6b7f396e0304965663dae1d1dd20cb
k1105 <kanapon1105@yahoo.co.jp>
2025-02-28 03:47:20 -0800

    footerの数字の表記条件を変更

--- components/common/Footer.tsx
+++ components/common/Footer.tsx
@@ -1,17 +1,26 @@
 import { useNameContainer } from "../context/NameContainerContext";
 import styles from "./Footer.module.scss";
 import { useSceneProps } from "./SceneManager";
+import { usePathname } from "next/navigation";
 
 export const Footer = ({ pageTitle }: { pageTitle: string }) => {
   const { currentSegmentIndex, segmentsLength, sceneIndex, sceneLength } =
     useSceneProps();
   const { isHidden } = useNameContainer();
+  useNameContainer(); // 現在のパスを取得
+  const pathname = usePathname();
+  const isWorkPath = pathname.startsWith("/work");
+  console.log("isWorkPath", isWorkPath);
+
   return (
     <div className={styles.footer}>
       <p>{pageTitle}</p>
 
       <div
-        style={{ opacity: isHidden ? 1 : 0, transition: "opacity 0.5s ease" }}
+        style={{
+          opacity: isHidden || !isWorkPath ? 1 : 0,
+          transition: "opacity 0.5s ease",
+        }}
       >
         {sceneLength > 1 ? (
           <p>
