f8c5aadcd0aff4cf9a8e139ae3f41fbae1760f35
k1105 <kanapon1105@yahoo.co.jp>
2025-03-03 16:12:21 +0900

    テキストの改行位置を修正

--- components/scene/bio/Bio.tsx
+++ components/scene/bio/Bio.tsx
@@ -4,6 +4,8 @@ import styles from "./Bio.module.scss";
 import { Fragment } from "react";
 import { InkFilter } from "../../common/filter/InkFilter";
 import { useSceneProps } from "@/components/common/SceneManager";
+import Typesetter from "palt-typesetting";
+import "palt-typesetting/dist/typesetter.css";
 
 export const Bio = ({ locale, bio }: { locale: string; bio: string }) => {
   const paragraphs = bio.split("\n").map((line, index) => (
@@ -14,6 +16,11 @@ export const Bio = ({ locale, bio }: { locale: string; bio: string }) => {
   ));
 
   const { transitionProgress } = useSceneProps();
+  // Typesetter のインスタンスを作成
+  const typesetter = new Typesetter();
+
+  // セレクターにマッチする要素に対して組版を適用
+  typesetter.renderToSelector("paragraph");
 
   return (
     <div className={styles.container}>
@@ -32,7 +39,9 @@ export const Bio = ({ locale, bio }: { locale: string; bio: string }) => {
           )}
         </p>
         <div className={styles.profileContainer}>
-          <p className={`${locale == "en" && styles.en}`}>{paragraphs}</p>
+          <p className={`${locale == "en" && styles.en} paragraph`}>
+            {paragraphs}
+          </p>
         </div>
       </InkFilter>
     </div>
